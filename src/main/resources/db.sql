b54369676f88216ec2cf7863a07ddf068a22f845
Eduardo Mendoza <eduardomendoza@Eduardos-MacBook-Pro.local>
2018-07-11 10:42:09 -0500

    database script

---
+++ src/main/resources/db.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE propellerhead;
+
+USE propellerhead;
+
+CREATE TABLE status(
+	id_status INT NOT NULL AUTO_INCREMENT,
+	status VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id_status)
+)ENGINE = MyISAM;
+
+INSERT INTO status (status) VALUES ('prospective'),('current'),('non-active');
+
+CREATE TABLE customer(
+	id_customer BIGINT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	id_status INT NOT NULL DEFAULT 1,
+	PRIMARY KEY (id_customer),
+	FOREIGN KEY (id_status) REFERENCES status(id_status) ON DELETE CASCADE ON UPDATE CASCADE 
+)ENGINE = MyISAM;
+
+INSERT INTO customer (name) VALUES ('Eduardo Mendoza Aguilar');
+
+CREATE TABLE email(
+	id_customer BIGINT NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	PRIMARY KEY (id_customer),
+	FOREIGN KEY (id_customer) REFERENCES customer(id_customer) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = MyISAM;
+
+INSERT INTO email VALUES (1,'eduardomendozamx@gmail.com');
+
+CREATE TABLE phone(
+	id_customer BIGINT NOT NULL,
+	phone VARCHAR(20),
+	PRIMARY KEY (id_customer),
+	FOREIGN KEY (id_customer) REFERENCES customer(id_customer) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = MyISAM;
+
+INSERT INTO phone VALUES (1,'+52 5527567675');
+
+CREATE TABLE address(
+	id_customer BIGINT NOT NULL,
+	address VARCHAR(500),
+	PRIMARY KEY (id_customer),
+	FOREIGN KEY (id_customer) REFERENCES customer(id_customer) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = MyISAM;
+
+INSERT INTO address VALUES (1,'St Real de los reyes D4, District Coyoacan, 04330 CDMX, MEXICO');
+
+CREATE TABLE note(
+	id_note BIGINT NOT NULL AUTO_INCREMENT,
+	id_customer BIGINT NOT NULL,
+	note VARCHAR(500),
+	PRIMARY KEY (id_note),
+	FOREIGN KEY (id_customer) REFERENCES customer(id_customer) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = MyISAM;
